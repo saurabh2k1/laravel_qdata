7423cd102d604701960c8dd1761dae97fb6bf920
Saurabh Sharma <saurabh2k1@gmail.com>
2020-01-31 16:24:24 +0530

    lastLogin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,8 +53,11 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
+                                
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    Last Login: {{ $user->lastLogin->timestamp->timezone('Asia/Kolkata') }}
+                                    <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
